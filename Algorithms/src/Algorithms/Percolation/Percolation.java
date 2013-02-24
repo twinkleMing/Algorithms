ab6ce8e5093def1a2599bb24e7cbb5e5f2b1f484
Ming Yang <yangming@ksu.edu>
2013-02-23 18:04:52 -0600

    assign1-1

--- Algorithms/src/Algorithms/Percolation/Percolation.java
+++ Algorithms/src/Algorithms/Percolation/Percolation.java
@@ -1,44 +1,89 @@
 package Algorithms.Percolation;
-import WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation 
 {
 	private int n;
-	boolean[] sites;
-	WeightedQuickUnionUF connections;
+	private boolean[] sites;
+	private int top;
+	private int bottom;
+	private int numopen;
+	private WeightedQuickUnionUF connections;
 	
 	public Percolation (int N) {
+		if ( N <= 0) 
+			throw new IndexOutOfBoundsException();
 		n = N;
 		sites = new boolean[n*n+2];
+		connections = new WeightedQuickUnionUF(sites.length);
 		for ( int i = 0; i < sites.length; i++)
 			sites[i] = false;
-		for ( int i = 0; i < n; i++)
-			sites[i]
+		top = n*n;
+		bottom = n*n+1;
+		numopen = 0;
 	}
 	
 	public void open ( int i, int j ) {
 		int index = convert2to1(i,j);
-		if (!sites[index]) {
-			sites[index] = true;
-			
-		}
+		if ( index == -1)
+			throw new IndexOutOfBoundsException();
+		if (sites[index]) 
+			return;
+		sites[index] = true;	
+		numopen++;
+		
+		int up = convert2to1(i-1, j);
+		if (up == -1)
+			connections.union(index, top);
+		else if (sites[up])
+				connections.union(index,up);
+		
+		int down = convert2to1(i+1, j);
+		if (down == -1) 
+			connections.union(index, bottom);
+		else if (sites[down])
+				connections.union(index,down);		
+		
+		int left = convert2to1(i, j-1);
+		if (left != -1 && sites[left])
+			connections.union(index,left);		
+		
+		int right = convert2to1(i, j+1);
+		if (right != -1 && sites[right])
+			connections.union(index,right);		
+		
 	}
 
 	public boolean isOpen ( int i, int j) {
+		int index = convert2to1(i,j);
+		if ( index == -1)
+			throw new IndexOutOfBoundsException();
 		
+		return sites[index];
 	}
 	
 	public boolean isFull (int i, int j) {
+		int index = convert2to1(i,j);
+		if ( index == -1)
+			throw new IndexOutOfBoundsException();
 		
+		return connections.connected(index, top);
 	}
 	
 	public boolean percolates() {
-		
+		return connections.connected(bottom, top);
 	}
 	
 	private int convert2to1( int i, int j) {
-		return n*i+j;
+		int index = n*(i-1)+(j-1);
+		if (index >= 0 && index <= n*n-1)
+			return index;
+		else return -1;
+	}
+	
+	public double fraction() {
+		return ((double) numopen) / ((double) n*n);
 	}
 	
 }
--- Algorithms/src/Algorithms/Percolation/PercolationStats.java
+++ Algorithms/src/Algorithms/Percolation/PercolationStats.java
@@ -1,12 +1,30 @@
 package Algorithms.Percolation;
-
+import edu.princeton.cs.introcs.StdRandom;
 public class PercolationStats 
 {
-	public PercolationStats ( int N, int T )
+	double[] results;
+	
+	public PercolationStats ( int N, int T ) {
+		results = new double[T];
+		for (int i = 0; i < T; i++) {
+			results[i] = simperco(N);
+		}
+	}
 	public double mean()
 	public double stddev()
 	public double confidenceLo()
 	public double confidenceHi()
 	public static void main(String[] args)
+	
+	private double simperco(int N) {
+		Percolation p = new Percolation(N);
+		
+		while (!p.percolates()) {
+			int i = StdRandom.uniform(N+1);
+			int j = StdRandom.uniform(N+1);
+			p.open(i, j);			
+		}
+		return p.fraction();
+	}
 	
 }
