ffb578b9ae2b52ae77f64f8d6e3af886af7c9ebd
Ming Yang <yangming@ksu.edu>
2013-03-22 13:56:01 -0500

    55

---
+++ Algorithms/src/KDTrees/Generator.java
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *  Compilation:  javac Generator.java
+ *  Execution:    java Generator N
+ *  Dependencies: 
+ *
+ *  Creates N random points in the unit square and print to standard output.
+ *
+ *  % java Generator 5
+ *  0.195080 0.938777
+ *  0.351415 0.017802
+ *  0.556719 0.841373
+ *  0.183384 0.636701
+ *  0.649952 0.237188
+ *
+ *************************************************************************/
+
+public class Generator {
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        for (int i = 0; i < N; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            System.out.printf("%8.6f %8.6f\n", x, y);
+        }
+    }
+}
